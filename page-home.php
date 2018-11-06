b3476214ba99f59a85263c8602aaa8ea0c5570ea
Danilo Miranda <danilomiranda@Danilos-MacBook-Pro.local>
2018-11-06 09:02:19 -0200

    Added ACF's repeater fields to manage the content from the restaurant's menu

--- page-home.php
+++ page-home.php
@@ -4,63 +4,25 @@
 
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<section class="container">
-	<h2 class="subtitulo">Menu da Semana</h2>
-
-	<div class="menu-item grid-8">
-		<h2>Peixes</h2>
-		<ul>
-			<li>
-				<span><sup>R$</sup>129</span>
-				<div>
-					<h3>Salmão Grelhado no Forno</h3>
-					<p>Pequenas tiras de salmão feitas no alho e óleo</p>
-				</div>
-			</li>
-			<li>
-				<span><sup>R$</sup>89</span>
-				<div>
-					<h3>Sardinha Frita na Cerveja</h3>
-					<p>Sardinhas escolhidas a dedo e fritas em cerveja premium</p>
-				</div>
-			</li>
-			<li>
-				<span><sup>R$</sup>159</span>
-				<div>
-					<h3>Camarão com Catupiry</h3>
-					<p>Grandes camarões grelhados, servidos ao molho de camarão com catupiry</p>
-				</div>
-			</li>
-		</ul>
-	</div>
-
-	<div class="menu-item grid-8">
-		<h2>Carnes</h2>
-		<ul>
-			<li>
-				<span><sup>R$</sup>129</span>
-				<div>
-					<h3>Picanha Nobre no Alho</h3>
-					<p>Pequenas tiras de salmão feitas no alho e óleo</p>
-				</div>
-			</li>
-			<li>
-				<span><sup>R$</sup>89</span>
-				<div>
-					<h3>Cupim no Bafo</h3>
-					<p>Sardinhas escolhidas a dedo e fritas em cerveja premium</p>
-				</div>
-			</li>
-			<li>
-				<span><sup>R$</sup>159</span>
-				<div>
-					<h3>Hamburger Artesanal Rest</h3>
-					<p>Grandes camarões grelhados, servidos ao molho de camarão com catupiry</p>
-				</div>
-			</li>
-		</ul>
-	</div>
-</section>
+	<section class="container">
+		<h2 class="subtitulo"><?php the_title() ?></h2>
+		<?php if ( have_rows('menu_category') ) : while ( have_rows('menu_category') ) : the_row(); ?>
+			<div class="menu-item grid-8">
+					<h2><?php the_sub_field('category_title') ?></h2>
+					<ul>
+						<?php if ( have_rows('category_content') ) : while ( have_rows('category_content') ) : the_row(); ?>
+							<li>
+								<span><sup>R$</sup><?php the_sub_field('item_price') ?></span>
+								<div>
+									<h3><?php the_sub_field('item_name') ?></h3>
+									<p><?php the_sub_field('item_description') ?></p>
+								</div>
+							</li>
+						<?php endwhile; else: endif; ?>
+					</ul>
+			</div>
+		<?php endwhile; else: endif; ?>
+	</section>
 <?php endwhile; else: ?>
 	<p>Sorry, no posts matched your criteria</p>
 <?php endif; ?>
